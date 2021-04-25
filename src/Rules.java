99b94a769dfa78ebd9408331ee1cc59a90eeef17
Cyril <kirill.lihotin@gmail.com>
2021-04-25 16:26:54 +0500

    finall

--- src/Rules.java
+++ src/Rules.java
@@ -17,31 +17,31 @@ public class Rules {
     public boolean win(char[] field, char sign, int i) {
         boolean flag = false;
         if(field[0] == sign & field[3] == sign & field[6] == sign) {
-            System.out.println("${sign} won");
+            System.out.println(sign + " won");
             flag = true;
         } else if(field[0] == sign & field[1] == sign & field[2] == sign) {
-            System.out.println("${sign} won");
+            System.out.println(sign + " won");
             flag = true;
         } else if(field[3] == sign & field[4] == sign & field[5] == sign) {
-            System.out.println("${sign} won");
+            System.out.println(sign + " won");
             flag = true;
         } else if(field[6] == sign & field[7] == sign & field[8] == sign) {
-            System.out.println("${sign} won");
+            System.out.println(sign + " won");
             flag = true;
         } else if(field[1] == sign & field[4] == sign & field[7] == sign) {
-            System.out.println("${sign} won");
+            System.out.println(sign + " won");
             flag = true;
         } else if(field[2] == sign & field[5] == sign & field[8] == sign) {
-            System.out.println("${sign} won");
+            System.out.println(sign + " won");
             flag = true;
         } else if(field[0] == sign & field[4] == sign & field[8] == sign) {
-            System.out.println("${sign} won");
+            System.out.println(sign + " won");
             flag = true;
         } else if(field[2] == sign & field[4] == sign & field[6] == sign) {
-            System.out.println("${sign} won");
+            System.out.println(sign + " won");
             flag = true;
         } else if(i == 8) {
-            System.out.println("${sign} won");
+            System.out.println(sign + " won");
             flag = true;
         }
         return flag;
